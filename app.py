654648955d4115cfde0dce81514f43a26e51a9d2
Rushil Goel <rushil.goel2019@vitstudent.ac.in>
2022-06-23 00:33:56 +0530

    Backend completed.

--- app.py
+++ app.py
@@ -28,3 +28,58 @@ class AddForm(FlaskForm):
 def index():
 	user_list = mongo.db.users.find()
 	return render_template("result.html",user_list=user_list)
+
+# Helper function to handle BSON as MongoDB supports BSON
+import json
+from bson import ObjectId
+from bson.objectid import ObjectId
+import bson 
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+# Create Document
+@app.route('/add', methods=["GET","POST"])
+def add():
+	form = AddForm()
+	if form.validate_on_submit():
+		fname_field = form.fname.data
+		lname_field = form.lname.data
+		phone_field = form.phone.data
+		data = ({'fname':fname_field, 'lname':lname_field, 'phone': phone_field})
+		user = mongo.db.users
+		user.insert_one(data)
+		return JSONEncoder().encode(data)
+	return render_template("add.html", form = form)
+
+# Update Page Route
+@app.route('/updateform')
+def updateform():
+	id = request.args.get('id')
+	user = mongo.db.users
+	result_id = user.find_one({'_id':ObjectId(id)})
+	form = AddForm(fname=result_id['fname'], lname=result_id['lname'], phone=result_id['phone'])
+	return render_template("update.html", form=form, id = id)
+
+# Update Document
+from bson import json_util
+@app.route('/update/<id>', methods=["POST"])
+def update(id):
+	user = mongo.db.users
+	form = AddForm()
+	if form.validate_on_submit():
+		result = user.update_one({'_id':ObjectId(id)},{'$set':{'fname':form.fname.data, 'lname':form.lname.data, 'phone': form.phone.data}})
+	return render_template("update.html",id=id,form=form)
+
+# Delete Document
+@app.route('/delete/<id>')
+def delete(id):
+	user = mongo.db.users
+	delete_record = user.delete_one({'_id':ObjectId(id)})
+	return redirect(url_for('index'))
+
+if __name__=='__main__':
+	app.run(debug=True)
